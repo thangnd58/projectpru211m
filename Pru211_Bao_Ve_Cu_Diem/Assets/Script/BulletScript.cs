d231e4601e0185546d528a49fbb12fa1b195d825
Nguyen Dinh Thang <thangga05082001@gmail.com>
2023-03-03 23:51:14 +0700

    git commit -m "PNS-31: Dan cua u sung"

---
+++ Pru211_Bao_Ve_Cu_Diem/Assets/Script/BulletScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+
+	Rigidbody2D rb;
+
+	[SerializeField]
+	GameObject explosionPrefab;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Ground")
+		{
+			Destroy(gameObject);
+			Instantiate<GameObject>(explosionPrefab, transform.position, Quaternion.identity);
+		}
+
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
