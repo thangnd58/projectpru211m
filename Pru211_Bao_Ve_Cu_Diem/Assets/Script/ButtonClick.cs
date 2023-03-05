4323a02df162beb07177bab8e7f35caa816db390
eigthnote72 <truong0989192304@gmail.com>
2023-03-05 14:17:46 +0700

    PNS-39 them scence + code click button tren man hinh chinh

---
+++ Pru211_Bao_Ve_Cu_Diem/Assets/Script/ButtonClick.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    [SerializeField]
+    Button buttonPlay;
+
+    [SerializeField]
+    Button buttonAmThanh;
+
+    [SerializeField]
+    Button buttonHuongDan;
+
+
+
+    public void playGame()
+    {
+        buttonPlay.interactable = true;
+        buttonAmThanh.interactable = true;
+        buttonHuongDan.interactable = true;
+        Time.timeScale = 1;
+    }
+
+    public void loadGame()
+    {
+        
+    }
+    public void setAmThanh()
+    {
+        
+    }
+    public void showHuongDan()
+    {
+
+    }
+
+}
