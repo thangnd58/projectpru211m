2dba99098bc0063bebc3e113abd8893ad95e776b
eigthnote72 <truong0989192304@gmail.com>
2023-03-08 23:58:41 +0700

    PNS-55 add code generator enermy

--- Pru211_Bao_Ve_Cu_Diem/Assets/Script/GenerateEnermy.cs
+++ Pru211_Bao_Ve_Cu_Diem/Assets/Script/GenerateEnermy.cs
@@ -16,19 +16,79 @@ public class GenerateEnermy : MonoBehaviour
     GameObject RobotEnermyPrefab;
     [SerializeField]
     GameObject TankEnermyPrefab;
+
+    Timer timer;
+    GenerateRound generateRound;
+    List<GameObject> listEnermy;
+
+    int countRound;
+
+
+    double countPower;
     // Start is called before the first frame update
     void Start()
     {
-        
-        Instantiate<GameObject>(InfrantryEnermyPrefab, posGround, Quaternion.identity);
-        Instantiate<GameObject>(AirForceEnermyPrefab, posAirForce, Quaternion.identity);
-        Instantiate<GameObject>(RobotEnermyPrefab, posGround, Quaternion.identity);
-        Instantiate<GameObject>(TankEnermyPrefab, posGround, Quaternion.identity);
+        countRound = 1; 
+        countPower = 0;
+        listEnermy = new List<GameObject>();
+        listEnermy.Add(InfrantryEnermyPrefab);
+        listEnermy.Add(AirForceEnermyPrefab);
+        listEnermy.Add(RobotEnermyPrefab);
+        listEnermy.Add(TankEnermyPrefab);
+
+        timer = gameObject.AddComponent<Timer>();
+        timer.Duration = 2f;
+        timer.run();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer.Finished)
+        {
+            if(countPower.CompareTo(generateRound.generateTotalEnemy(countRound)) >0)
+            {
+                spawnEnermy();
+                Debug.Log(countPower);
+            }
+            else
+            {
+                Debug.Log("Done");
+                if(listEnermy.Count == 0)
+                {
+                    countPower = 0;
+                    countRound++;
+                }
+            }
+            
+            timer.run();
+            
+        }
+    }
+
+    public void spawnEnermy()
+    {
+        int site = Random.Range(0,listEnermy.Count);
+        if (listEnermy[site] == AirForceEnermyPrefab)
+        {
+            countPower += 3;
+            Instantiate<GameObject>(AirForceEnermyPrefab, posAirForce, Quaternion.identity);
+        }
+        else if (listEnermy[site] == InfrantryEnermyPrefab)
+        {
+            countPower += 1;
+            Instantiate<GameObject>(InfrantryEnermyPrefab, posGround, Quaternion.identity);
+        }
+        else if (listEnermy[site] == RobotEnermyPrefab)
+        {
+            countPower += 7;
+            Instantiate<GameObject>(RobotEnermyPrefab, posGround, Quaternion.identity);
+
+        }
+        else if (listEnermy[site] == TankEnermyPrefab)
+        {
+            countPower += 10;
+            Instantiate<GameObject>(TankEnermyPrefab, posGround, Quaternion.identity);
+        }
     }
 }
